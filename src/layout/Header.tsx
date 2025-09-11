86a7eaafb9f32b43d02f915cc73b281229748808
Анастасия Бойко <anastasia151000@rambler.ru>
2025-09-11 14:37:45 +0300

    header mobile

--- src/layout/Header.tsx
+++ src/layout/Header.tsx
@@ -1,25 +1,40 @@
 import { useState } from 'react';
 
+// components
 import { Logo } from '../components/logo/Logo';
 import { SearchInput } from '../components/ui/searchInput/SearchInput';
 import { ModalComponent } from '../components/modal/ModalComponent';
 import { HeaderMenu } from '../components/headerMenu/HeaderMenu';
 import { ButtonLogIn } from '../components/ui/button/ButtonLogIn';
+
+// hooks
+import { useDeviceDetect } from '../hooks/useDeviceDetect';
+
+// mui
 import Avatar from '@mui/material/Avatar';
+import { IconButton } from '@mui/material';
+import MenuIcon from '@mui/icons-material/Menu';
 
 export const Header = () => {
   const [open, setOpen] = useState(false);
   const handleOpen = () => setOpen(true);
+  const { isMobile } = useDeviceDetect();
 
   return (
     <header className="header">
       <div className="header__container container">
         <div className="header__block">
           <Logo />
-          <HeaderMenu />
+          {isMobile ? (
+            <IconButton>
+              <MenuIcon sx={{ color: 'white' }} fontSize="large" />
+            </IconButton>
+          ) : (
+            <HeaderMenu />
+          )}
         </div>
         <div className="header__block">
-          <SearchInput />
+          {isMobile ? <></> : <SearchInput />}
           <ButtonLogIn onClick={handleOpen} />
           <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
         </div>
