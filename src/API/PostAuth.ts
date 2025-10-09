da9843bd71ed300f81028bc82aafcd54afefa829
Анастасия Бойко <anastasia151000@rambler.ru>
2025-10-09 21:35:13 +0300

    api change

--- src/API/PostAuth.ts
+++ src/API/PostAuth.ts
@@ -1,6 +1,6 @@
 export const PostAuth = async ({ emailOrUsername, password }: { emailOrUsername: string; password: string }) => {
   try {
-    const response = await fetch(process.env.REACT_APP_API_URL!, {
+    const response = await fetch(`${process.env.REACT_APP_API_URL}/login`, {
       method: 'POST',
       body: JSON.stringify({ email: emailOrUsername, password: password }),
     });
--- src/API/PostReg.ts
+++ src/API/PostReg.ts
@@ -1,6 +1,6 @@
 export const PostReg = async ({ username, email, password }: { username: string; email: string; password: string }) => {
   try {
-    const response = await fetch(process.env.REACT_APP_API_URL!, {
+    const response = await fetch(`${process.env.REACT_APP_API_URL}/register`, {
       method: 'POST',
       body: JSON.stringify({ nickname: username, email: email, password: password }),
     });
