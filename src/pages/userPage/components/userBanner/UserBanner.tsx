82ebca62772d08e0835fce91a56ad70b9e1a2b3f
Анастасия Бойко <anastasia151000@rambler.ru>
2025-11-30 19:33:02 +0300

    userImage

--- src/pages/userPage/components/userBanner/UserBanner.tsx
+++ src/pages/userPage/components/userBanner/UserBanner.tsx
@@ -42,7 +42,7 @@ export const UserBanner = ({ userData, isNotProfileData }: any) => {
   };
 
   useEffect(() => {
-    console.log(`${process.env.REACT_APP_API_USER}${userData.profileImage}`);
+    // console.log(`${process.env.REACT_APP_API_USER}${userData.profileImage}`);
     checkIsSubscribed();
   }, [subscribers]);
 
@@ -90,7 +90,7 @@ export const UserBanner = ({ userData, isNotProfileData }: any) => {
       {!userData?.backgroundImage && <BannerEffect />}
       <StyledBannerAvatar
         src={
-          userData?.profileImage ? `${process.env.REACT_APP_API_USER}${userData.profileImage}` : '/default-avatar.jpg'
+          userData?.profileImage ? `${process.env.REACT_APP_API_USER}${userData?.profileImage}` : '/default-avatar.jpg'
         }
       />
       <Socials socials={currentUser?.socialLinks} />
